509bf230dabe8ee0792c385f5388fe3358a08353
Enno Welbers <enno.welbers@googlemail.com>
2015-08-20 11:47:28 +0200

    made frontend rendering work

---
+++ src/Controller/GridFrontendController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enno
+ * Date: 20.08.15
+ * Time: 10:51
+ */
+
+namespace Drupal\grid\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+class GridFrontendController
+{
+    public function css()
+    {
+        global $grid_lib;
+        return new Response($grid_lib->getContainerSlotCSS(db_query("SELECT * FROM {grid_container_type}")));
+    }
+}
