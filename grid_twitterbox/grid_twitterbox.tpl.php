ce96e8a8379286c84f9c10f2caf73446c29069ac
Kim Meyer <kim.meyer@palasthotel.de>
2017-04-12 14:16:48 +0200

    twitterbox refactoring, removed php shorttags

--- grid_twitterbox/grid_twitterbox.tpl.php
+++ grid_twitterbox/grid_twitterbox.tpl.php
@@ -1,10 +1,5 @@
-<?
-	foreach($content as $tweet)
-	{
-?>
-<div>
-	<?= "--<br />".$tweet->text?>
-</div>
-<?
-	}
-?>
+<?php foreach($content as $tweet): ?>
+  <div>
+    <?php echo "--<br />" . $tweet->text; ?>
+  </div>
+<?php endforeach; ?>
