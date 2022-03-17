ec4707ef83c8a6960d9c47f64c9954a20a46791c
Sebastian Srb <sebastian.srb@palasthotel.de>
2022-03-17 17:18:50 +0100

    Rename folder-path

--- src/TwoClick/Constants/Constants.php
+++ src/TwoClick/Constants/Constants.php
@@ -7,6 +7,6 @@ class Constants {
   const PROVIDER_YOUTUBE = "YouTube";
   const PROVIDER_VIMEO = "Vimeo";
   const VIMEO_API_KEY = "6cfada00285024bfe0dc2b48dfc641b7";
-  const THUMBNAIL_FOLDER_PATH = "public://twoclickthumbnails/";
+  const THUMBNAIL_FOLDER_PATH = "public://TwoclickThumbnails/";
 
 }
